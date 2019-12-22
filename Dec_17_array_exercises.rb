192e5ff68de4265fbd4eb49c2d69b10de3a0df6e
Jacobleakley <jacob.bleakley@gmail.com>
2019-12-22 11:08:27 -0500

    array exercises #3

---
+++ Dec_17_array_exercises.rb
@@ -0,0 +1,67 @@
+expenses = [250, 7.95, 30.95, 16.50]
+more_expenses = [1000,100.1, 400, 7.35, 1000]
+
+# sum = expenses.sum
+# print sum 
+
+# def sum(arr) 
+
+#     amt = 0.0
+#     arr.each do |i|
+#     amt += i
+#     end 
+#     return amt
+# end 
+# puts sum(more_expenses)
+
+### EXERCISE 2 #####
+
+grocery_list = ["pasta", "soap", "yogurt", "cheese"]
+
+#QUESTION 1
+# grocery_list.each do |i|
+#     puts "* #{i}" 
+# end 
+
+#QUESTION 1*
+
+# grocery_list[4] = "rice"
+
+# def shopping(arr)
+#     arr.each do |i|
+#     puts "* #{i}" 
+#     end 
+# end 
+
+# shopping(grocery_list)
+
+#QUESTION 2
+
+# puts grocery_list.count
+
+#QUESTION 3
+
+# if grocery_list.include? 'bananas' then 
+#     puts "You need to pick up bananas today. " else
+#     puts "You don't need to pick up bananas today. "
+# end 
+
+# QUESTION 4 
+
+# print grocery_list[1]
+
+#QUESTION 5
+
+grocery_list[4] = "rice"
+
+# grocery_list.sort.each do |i|
+# puts "* #{i}"
+# end 
+
+# QUESTION 6
+
+grocery_list.pop
+
+grocery_list.sort.each do |i|
+    puts "* #{i}"
+end   
